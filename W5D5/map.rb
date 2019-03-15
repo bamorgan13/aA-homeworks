31383b84a6b314c672c53be95e0398026717ec54
Bryce Morgan <bamorgan13@gmail.com>
2019-03-14 22:57:44 -0400

    Implement initialize, set, and get methods of map

--- W5D5/map.rb
+++ W5D5/map.rb
@@ -1,15 +1,22 @@
 class Map
 
     def initialize
-
+        @map = []
     end
 
     def set(key, value)
-
+        @map.each_with_index do |ele, idx|
+            if ele[0] == key
+                @map[idx][1] = value
+                return @map
+            end
+        end
+        @map << [key, value]
     end
 
-    def get(key, value)
-
+    def get(key)
+        @map.each_with_index { |ele, idx| return ele[1] if ele[0] == key }
+        nil
     end
 
     def delete(key)
@@ -17,7 +24,7 @@ class Map
     end
 
     def show
-
+        
     end
 
 end
